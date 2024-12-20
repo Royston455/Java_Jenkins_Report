f9ed983a8021ce2f4e8f20c0d53cdce86335149b
Royston Dsouza <roystandsouza1996@gmail.com>
2024-12-20 05:18:02 -0800

    sdsad

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import org.example.Main;
+import org.junit.Test;
+
+public class HelloWorldTest
+{
+@Test
+    public void test (){
+
+    System.out.println("Hello world!");
+    System.out.println("Yes");
+    }
+}
