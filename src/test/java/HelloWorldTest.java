b67ba0d68944a500b939bbb7aa15aa6e96708653
Royston Dsouza <roystandsouza1996@gmail.com>
2024-12-20 22:58:37 -0800

    sdsad

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -4,9 +4,10 @@ import org.junit.Test;
 public class HelloWorldTest
 {
 @Test
-    public void test (){
+    public void test () throws Exception {
 
     System.out.println ("Hello worldo!");
     System.out.println("Yes");
+    throw new Exception("unable to print");
     }
 }
